45d2a130322f57acb4ac2889c11aff7a565f98e6
Davozay <doayozie@gmail.com>
2025-07-01 02:01:58 +0100

    remove binary and compiled files

--- backend/issue.py
+++
@@ -1,9 +0,0 @@
-import os
-
-for root, dirs, files in os.walk("."):
-    for f in files:
-        try:
-            with open(os.path.join(root, f), encoding="utf-8") as test_file:
-                test_file.read()
-        except Exception as e:
-            print(f"Problem with {f}: {e}")
